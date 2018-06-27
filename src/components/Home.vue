89de5c45ee229c007456761ac12eb28a41359d1b
Noah Bystrom <noah.bystrom@calbt.com>
2018-06-27 09:40:48 -0700

    Fix message display
    Displays user messages differently to received ones
    Mobile-friendly message display

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,26 +4,30 @@
       <v-flex xs12 class="text-xs-center" mt-5>
         <h1>Home Page for the Chat App</h1>
       </v-flex>
-      <v-flex xs4>
-        <ul>
-          <li v-for="message in messages" :key="message.message">
+      <v-flex xs12 sm8 md6 offset-sm2 offset-md3 class="text-xs-center">
+        <div class="row">
+          <span :class="{ 'message user-message': isUser(message.sender), 'message received-message': !isUser(message.Sender) }" v-for="message in messages" :key="message.message">
             <small><b>{{ message.sender }}</b></small><br>
             {{ message.text }} <br>
             <small>{{ ParseDate(message.time) }}</small>
-          </li>
-        </ul>
+          </span>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data () {
     var date = new Date()
     return {
       messages: [
-        { text: 'this is a message', sender: 'penelope', time: date }
+        { text: 'this is a message', sender: 'penelope', time: date },
+        { text: 'a message', sender: 'naiomi', time: date },
+        { text: 'a rather much longer one', sender: 'penelope', time: date }
       ]
     }
   },
@@ -32,19 +36,40 @@ export default {
       var d = date.toISOString().slice(0, 16).replace('T', ' ')
       d = d.replace(/-/g, '/')
       return d
+    },
+    isUser (sender) {
+      if (sender === this.user) {
+        return true
+      } else {
+        return false
+      }
     }
+  },
+  computed: {
+    ...mapState([
+      'user'
+    ])
   }
 }
 </script>
 
 <style>
-  ul {
-    list-style-type: none;
-    font-size: 1.25em;
-  }
-  li {
-    background: white;
+  .message {
+    font-size: 1.2em;
     padding: 1rem 1.5rem;
-    display: inline-block;
+    margin: 5px;
+    display: table;
+    position: relative;
+    clear: both;
+  }
+  .received-message {
+    background: white;
+    text-align: left;
+    float: left;
+  }
+  .user-message {
+    background: lightgrey;
+    text-align: right;
+    float: right;
   }
 </style>
